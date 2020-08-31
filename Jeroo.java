9424035d8dcfb0f3c4c4cdfef930524f912ceb0b
kowalski-23 <69799539+kowalski-23@users.noreply.github.com>
2020-08-31 09:46:16 -0500

    Update Jeroo.java

--- Jeroo.java
+++ Jeroo.java
@@ -5,13 +5,48 @@
 public class Jeroo extends JerooBase {
 
     
-    public void plantCross() {
-        
+    public void plantCross() 
+    {
+        this.side();
+        this.side();
+        this.side();
+        this.side();
     }
     // Put any helpermethods here
     
-    
-    
+    public void plantHop()
+    {
+      this.plant();
+      this.hop();
+    }
+
+    public void side()
+    {
+     this.corner();
+     this.shortLine();
+    }
+
+    public void corner()
+    {
+     this.line();
+     this.turn(LEFT);
+     this.line();
+    }
+
+    public void line()
+    {
+     this.plantHop();
+     this.plantHop();
+     this.plantHop();
+    }
+
+    public void shortLine()
+    {
+     this.turn(RIGHT);
+     this.plantHop();
+     this.plantHop();
+     this.turn(RIGHT);
+    }
     
     // Do NOT touch the code below here
 
